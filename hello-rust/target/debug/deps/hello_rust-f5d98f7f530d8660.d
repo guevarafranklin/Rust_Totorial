8875570fd26e643056bbfca76e19fac286d94d5c
Franklin Guevara <tecguevara@gmail.com>
2020-08-21 18:50:17 -0600

    My second commit

---
+++ hello-rust/target/debug/deps/hello_rust-f5d98f7f530d8660.d
@@ -0,0 +1,5 @@
+/home/fguevara/MyRepos/Rust_Totorial/hello-rust/target/debug/deps/hello_rust-f5d98f7f530d8660: src/main.rs
+
+/home/fguevara/MyRepos/Rust_Totorial/hello-rust/target/debug/deps/hello_rust-f5d98f7f530d8660.d: src/main.rs
+
+src/main.rs:
---
+++ hello-rust/target/debug/hello-rust.d
@@ -0,0 +1,1 @@
+/home/fguevara/MyRepos/Rust_Totorial/hello-rust/target/debug/hello-rust: /home/fguevara/MyRepos/Rust_Totorial/hello-rust/src/main.rs
